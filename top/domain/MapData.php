3af99eb5b7fa98564c0f17780b9665ea8468695e
shanezhiu <shanezhiu@gmail.com>
2020-10-08 10:38:43 +0800

    init tbk union project

---
+++ top/domain/MapData.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * data
+ * @author auto create
+ */
+class MapData
+{
+	
+	/** 
+	 * 内容列表
+	 **/
+	public $contents;
+	
+	/** 
+	 * 返回条数
+	 **/
+	public $count;
+	
+	/** 
+	 * 最后一条内容的发布时间
+	 **/
+	public $last_timestamp;	
+}
+?>
